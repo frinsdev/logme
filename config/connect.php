660b20e5f1a0df735eea402ad8460b649aedc55f
Prince Karlo <46485646+princekarlo@users.noreply.github.com>
2021-09-12 09:21:11 +0800

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-  $db_host = 'sql103.epizy.com';
-  $db_username = 'epiz_29573972'; 
-  $db_password = 'QYwB6TWdPN';
-  $db_name = 'epiz_29573972_logme';
+  $db_host = '';
+  $db_username = ''; 
+  $db_password = '';
+  $db_name = '';
 
   $conn = mysqli_connect($db_host, $db_username, $db_password);
   $db_select = mysqli_select_db($conn, $db_name);
